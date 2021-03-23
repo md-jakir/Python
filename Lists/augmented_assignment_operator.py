0b4e91e665f3aa00e865d8efce2575f2a2b5030b
Jakir Hossain <md-jakir@users.noreply.github.com>
2021-03-23 15:35:23 +0600

    Create augmented_assignment_operator.py

---
+++ Lists/augmented_assignment_operator.py
@@ -0,0 +1,27 @@
+# Augmented assignment operator is for increment, decrement, multiplication
+
+name = ['Lio']
+name *= 3
+name
+['Lio', 'Lio', 'Lio']
+
+# Finding a value in a list with index() function
+
+country = ['BD', 'US', 'UK', 'Japan']
+country.index('US')
+1
+
+# Adding value in a list with append() and insert() 
+# append() function will add value at the end of the list
+# insert() function will add value at any location in a list
+
+animals = ['cat', 'bat', 'rat']
+animals.append('mouse')
+animals
+['cat', 'bat', 'rat', 'mouse']
+
+# insert()
+animals = ['cat', 'bat', 'rat']
+animals.insert(1, 'elephant')
+animals
+['cat', 'elaphant', 'bat', 'rat']
