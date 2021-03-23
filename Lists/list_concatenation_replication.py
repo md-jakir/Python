282d4d6c1b4b6eef976aecd95f2bad1a60d4b44e
Jakir Hossain <md-jakir@users.noreply.github.com>
2021-03-23 11:42:34 +0600

    Create list_concatenation_replication.py

---
+++ Lists/list_concatenation_replication.py
@@ -0,0 +1,13 @@
+# List concatenation is adding two list using + operator
+
+['cat', 'bat', 'mouse'] + [10, 20, 30, 40,50]
+['cat', 'bat', 'mouse', 10, 20, 30, 40, 50]
+
+animals = ['cat', 'bat']
+number = [10, 20, 30]
+total = animals + number
+
+# Removing values from List using del statement
+
+animals = ['cat', 'bat', 'rat', 'monkey']
+del animals[1]
