0f2b8602cc02546378956998f6d99b8f7f838562
Jakir Hossain <md-jakir@users.noreply.github.com>
2021-03-23 14:47:45 +0600

    Create for_loop_list.py

---
+++ Lists/for_loop_list.py
@@ -0,0 +1,18 @@
+# for loops with list
+
+for i in range(5):
+  pring(i)
+
+ output: 
+  0
+  1
+  2
+  3
+  4
+  5
+# Another program with for loops 
+country = ['BD', 'US', 'UK', 'Japan']
+for i in range(len(country)):
+  print('Índex ' + str(i) + 'in country is: ' + country[i]) # here str(i) will provide '0', '1' ........such as that will concatenate as string. 
+  
+  
