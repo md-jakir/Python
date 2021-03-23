dacbb70252cb42d3431bdb8d1102fa6d8b30a986
Jakir Hossain <md-jakir@users.noreply.github.com>
2021-03-23 16:41:47 +0600

    Create sorting_list.py

---
+++ Lists/sorting_list.py
@@ -0,0 +1,11 @@
+# sorting the number of list or string list using sort() function
+
+numbers = [10, 2, 0, -1, -2, 6, 8, 3]
+numbers.sort()
+numbers
+[-2, -1, 0, 2, 3, 6, 8, 10]
+
+# Mutable and Immutable
+# in a List can modify the index's value this is mutable and in a string can't change value is called immutable
+# tuple is immutable and written with () instead of []. 
+          
