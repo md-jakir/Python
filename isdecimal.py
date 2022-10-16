7f73a0736c5ba511f8efce4382a64527ba03f965
md-jakir <mdavir814@gmail.com>
2022-10-16 16:52:01 +0600

    update isdecimal

--- isdecimal.py
+++ isdecimal.py
@@ -1,6 +1,16 @@
+from mimetypes import init
+
+
 while True:
     print('Enter your age:')
     age = input()
     if age.isdecimal():
         break
     print('Select a number for you age.')
+
+while True:
+    print('Select a new password (Letter & number only):')
+    passwd = input()
+    if passwd.isalnum(): # Only letters or only number or both are combined are accecptable
+        break
+    print('Password can only have letters & numbers')
